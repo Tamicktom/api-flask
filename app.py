44973a1204ad85d19a4ebe40d4c28d4d792876c1
Tamicktom <rickpokemon38@gmail.com>
2024-03-20 22:17:29 -0300

    get route

--- app.py
+++ app.py
@@ -16,14 +16,23 @@ def create_task():
     global task_id_control
     data = request.get_json()
     new_task = Task(
-        id=str(task_id_control),
+        id=task_id_control,
         title=data.get("title", "No title"),
         description=data.get("description", "")
     )
     task_id_control += 1
     tasks.append(new_task)
-    print(tasks)
     return jsonify({"message": "New task created successfully"}), 201
+
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    output = {
+        "tasks": [task.to_dict() for task in tasks],
+        "total_tasks": len(tasks)
+    }
+
+    return jsonify(output), 200
 
 
 if __name__ == '__main__':
--- models/task.py
+++ models/task.py
@@ -6,7 +6,7 @@ TaskType = namedtuple('TaskType', ['id', 'title', 'description', 'completed'])
 class Task:
     data: TaskType
 
-    def __init__(self, id: str, title: str, description: str, completed: bool = False) -> None:
+    def __init__(self, id: int, title: str, description: str, completed: bool = False) -> None:
         self.data = TaskType(id, title, description, completed)
 
     def to_dict(self) -> TaskType:
