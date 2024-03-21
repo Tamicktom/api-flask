2b0ec759ab8c3da225ea14c61338cd475f787ae5
Tamicktom <rickpokemon38@gmail.com>
2024-03-20 22:10:53 -0300

    saving

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Libraries imports
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from typing import List
 
 # Local imports
@@ -8,13 +8,22 @@ from models.task import Task, TaskType
 app = Flask(__name__)
 
 tasks: List[TaskType] = []
+task_id_control: int = 1
 
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return "Task created"
+    new_task = Task(
+        id=str(task_id_control),
+        title=data.get("title", "No title"),
+        description=data.get("description", "")
+    )
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "New task created successfully"}), 201
 
 
 if __name__ == '__main__':
