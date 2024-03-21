257c41819b125c7352683ff91c8fe4ca80e25357
Tamicktom <rickpokemon38@gmail.com>
2024-03-20 21:57:08 -0300

    saving

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+# Libraries imports
+from flask import Flask, request
+from typing import List
+
+# Local imports
+from models.task import Task, TaskType
 
 app = Flask(__name__)
 
+tasks: List[TaskType] = []
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return "Task created"
 
 
 if __name__ == '__main__':
---
+++ models/task.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+TaskType = namedtuple('TaskType', ['id', 'title', 'description', 'completed'])
+
+
+class Task:
+    data: TaskType
+
+    def __init__(self, id, title, description, completed=False) -> None:
+        self.data = TaskType(id, title, description, completed)
+
+    def to_dict(self) -> TaskType:
+        return self.data
